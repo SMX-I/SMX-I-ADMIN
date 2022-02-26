f2234189b210cd566c5683f36156034e3a47fc37
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-02-26 13:47:51 +0800

    ..

--- Settings.lua
+++ Settings.lua
@@ -1,1 +1,21 @@
+--[
 
+-- You can edit these scripts by forking.
+
+-- Set your own ranks
+
+----------------------
+
+-- Set your own commands.
+
+-----------------------
+
+-- Settings
+
+Prefix = "> "           | -- The prefix for all comamnds. Example for: If the prefix is set to "> ", you will use the comamnds like "> cmds".
+SplitKey = " "          | -- If set to "/", comamnds eill become "> fly me".
+BatchKey = ""           | -- Unavailible
+BatchQuallifierKey = "" | -- Unavailible
+
+
+-- * All rights reserved. © SMX - I | Created 2022 Feb 26 MYT/GMT+8
